b848c791039f5969207074240bb4d5219e9a5f3d
jeremyjarrell <jeremyjarrell@2772a39f-f33f-0410-bc57-f9774f0e92b5>
2008-06-23 16:59:12 +0000

    Tied the JesterPresenter to the Console view.
    git-svn-id: https://jesterdotnet.svn.sourceforge.net/svnroot/jesterdotnet@35 2772a39f-f33f-0410-bc57-f9774f0e92b5

--- JesterDotNet.UI.Console/Program.cs
+++ JesterDotNet.UI.Console/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using JesterDotNet.Presenter;
+﻿using JesterDotNet.Presenter;
 
 namespace JesterDotNet.UI.Console
 {
@@ -8,30 +7,21 @@ namespace JesterDotNet.UI.Console
         private static void Main(string[] args)
         {
             var consoleView = new JesterConsoleView();
+            var presenter = new JesterPresenter(consoleView);
+            presenter.MutationComplete += OnPresenterMutationComplete;
+            presenter.TestComplete += OnPresenterTestComplete;
+
             consoleView.RunMutation(args[0], args[1]);
         }
-    }
 
-    internal class JesterConsoleView : IJesterView
-    {
-        /// <summary>
-        /// Occurs when the user has initiated the full mutation and test process.
-        /// </summary>
-        public event EventHandler<RunEventArgs> Run;
-
-        /// <summary>
-        /// Gets a value indicating whether user has cancelled the mutation.
-        /// </summary>
-        /// <value><c>true</c> if the user has cancelled the mutation; otherwise, <c>false</c>.</value>
-        public bool CancellationPending
+        static void OnPresenterTestComplete(object sender, System.EventArgs e)
         {
-            get { throw new NotImplementedException(); }
+            
         }
 
-        public void RunMutation(string targetAssembly, string testAssembly)
+        static void OnPresenterMutationComplete(object sender, MutationCompleteEventArgs e)
         {
-            if (Run != null)
-                Run(this, new RunEventArgs(targetAssembly, testAssembly, null));
+            
         }
     }
 }
